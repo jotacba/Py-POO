150b52102c9f563f7b607d62ed7472dbdf2cbe5b
Joel Ramos <jotadelramos@gmail.com>
2024-02-26 09:51:29 -0300

    nuevo

--- pruebas/gpu.py
+++ pruebas/gpu.py
@@ -3,6 +3,8 @@ from enum import Enum
 class Fabricante(Enum):
     NVIDIA = "NVIDIA"
     AMD = "AMD"
+    SAMSUNG = "SAMSUNG"
+    MOTOROLA = "MOTOROLA"
 
 class Condicion(Enum):
     NUEVO = "Nuevo"
@@ -54,9 +56,47 @@ class Gpu(Hardware):
             self.vida -= 1
         print("La placa ya no tiene vida, 'esta muerta'")
 
+class Compania(Enum):
+    CLARO = "CLARO"
+    PERSONAL = "PERSONAL"
+    MOVISTAR = "MOVISTAR"
+
+class Sistema_operativo(Enum):
+    ANDROID = "ANDROID"
+    IOS = "IOS"
+
+class Celular(Hardware):
+    def __init__(self, fabricante, condicion, estado, color, precio, compania, sistema_operativo, pixeles, contactos):
+        super().__init__(fabricante, condicion, estado, color, precio)
+        self.compania = compania
+        self.sistema_operativo = sistema_operativo
+        self.pixeles = pixeles
+        self.contactos = contactos
+
+    def mostrar_celular(self):
+        print(f'Fabricante: {self.fabricante}')
+        print(f'Condicion: {self.condicion}')
+        print(f'Estado: {self.estado}')
+        print(f'Color: {self.color}')
+        print(f'Precio: {self.precio}')
+        print(f'Compania: {self.compania}')
+        print(f'Sistema Operativo: {self.sistema_operativo}')
+        print(f'Pixeles de la camara: {self.pixeles}')
+        print(f'Lista de contactos: {self.contactos}')
+    
+    def agregar_numero(self, nombre, numero):
+        self.contactos[nombre] = numero
+        
 placa1 = Gpu(Fabricante.NVIDIA.value, Condicion.USADO.value, Estado.LISTO.value, "Negro", 
              "$5000", "4 GB", 1, "1920x1080", 5)
-placa1.mostrar_gpu()
-placa1.reparar()
-placa1.usar()
-placa1.mostrar_gpu()
+#placa1.mostrar_gpu()
+#placa1.reparar()
+#placa1.usar()
+#placa1.mostrar_gpu()
+
+celular1 = Celular(Fabricante.SAMSUNG.value, Condicion.USADO.value, Estado.LISTO.value, "Plateado", 569000,
+                   Compania.CLARO.value, Sistema_operativo.ANDROID.value, 50, {})
+
+celular1.mostrar_celular()
+celular1.agregar_numero("Albeto", 3517788999)
+celular1.mostrar_celular()
